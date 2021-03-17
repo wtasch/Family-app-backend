7b1c88ea9df44675f5c786fd702a680b49a76af6
wtasch <wtasch@aol.com>
2021-03-17 15:34:38 -0400

    deployment issue

--- server.js
+++ server.js
@@ -32,43 +32,43 @@ app.use(bodyParser.json());
 
 //add for cf
 
-const contactEmail = nodemailer.createTransport({
-    host: "smtp.aol.com",
-    port: 587,
-    auth: {
-      user: "wtasch",
-      pass: "Rocketman69",
-    },
-  });
+// const contactEmail = nodemailer.createTransport({
+//     host: "smtp.aol.com",
+//     port: 587,
+//     auth: {
+//       user: "wtasch",
+//       pass: "Rocketman69",
+//     },
+//   });
   
-  contactEmail.verify((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready to Send");
-    }
-  });
+//   contactEmail.verify((error) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("Ready to Send");
+//     }
+//   });
 
-  router.post("/contact", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;console.log(email)
-    const message = req.body.message; 
-    const mail = {
+//   router.post("/contact", (req, res) => {
+//     const name = req.body.name;
+//     const email = req.body.email;console.log(email)
+//     const message = req.body.message; 
+//     const mail = {
         
-      from: name,
-      to: "wtasch@aol.com",
-      subject: "Contact Form Message",
-      html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
-    };
-    console.log(mail)
-    contactEmail.sendMail(mail, (error) => {
-      if (error) {
-        res.json({ status: "failed" });
-      } else {
-        res.json({ status: "sent" });
-      }
-    });
-  });
+//       from: name,
+//       to: "wtasch@aol.com",
+//       subject: "Contact Form Message",
+//       html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+//     };
+//     console.log(mail)
+//     contactEmail.sendMail(mail, (error) => {
+//       if (error) {
+//         res.json({ status: "failed" });
+//       } else {
+//         res.json({ status: "sent" });
+//       }
+//     });
+//   });
 
 
 const verifyToken = (req, res, next) => {
